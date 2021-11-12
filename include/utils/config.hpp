b48c307897e39e84c6b8553dd5dd99523bf04dba
romainducrocq <romain.g.ducrocq@gmail.com>
2021-11-12 02:24:47 +0100

    init

--- include/utils/config.hpp
+++ include/utils/config.hpp
@@ -20,32 +20,32 @@ struct DefaultConf
     static bool argParse(int argc, char** argv)
     {
         for(;;){
-            switch(getopt(argc, argv, "hr:")){
+            switch(getopt(argc, argv, "hv:")){
                 case '?':
                 case 'h':
                 default :
-                    std::cerr << "usage: apps/exec [-h] [-r RES]\n";
+                    std::cerr << "usage: apps/exec [-h] [-v VP]\n";
                     std::cerr << "\n";
                     std::cerr << DefaultConf<T>::NAME << "\n";
                     std::cerr << "\n";
                     std::cerr << "optional args:\n";
-                    std::cerr << "  -h      show this help message and exit\n";
-                    std::cerr << "  -r RES  set resolution < 480p | 720p > \n";
+                    std::cerr << "  -h     show this help message and exit\n";
+                    std::cerr << "  -v VP  set viewport size < 480 | 720 >\n";
                     std::cerr << "\n";
                     std::cerr << "optional cmds:\n";
-                    std::cerr << "  key S   set speed 24FPS, VSYNC, MAX    \n";
-                    std::cerr << "  key D   set debug NO, YES              \n";
+                    std::cerr << "  key S  set speed 24FPS, VSYNC, MAX    \n";
+                    std::cerr << "  key D  set debug NO, YES              \n";
 
                     return false;
 
                 case -1:
                     break;
 
-                case 'r':
-                    if(std::strcmp(optarg, "480p") == 0){
+                case 'v':
+                    if(std::strcmp(optarg, "480") == 0){
                         DefaultConf<T>::WIN_W = 720;
                         DefaultConf<T>::WIN_H = 480;
-                    }else if(std::strcmp(optarg, "720p") == 0){
+                    }else if(std::strcmp(optarg, "720") == 0){
                         DefaultConf<T>::WIN_W = 1280;
                         DefaultConf<T>::WIN_H = 720;
                     }
@@ -60,7 +60,7 @@ struct DefaultConf
 };
 
 template<typename T>
-const char* DefaultConf<T>::NAME = "SFML-TEMPLATE";
+const char* DefaultConf<T>::NAME = "SFML-Template";
 template<typename T>
 const int DefaultConf<T>::BG_COL[3] = { 51, 51, 51 };
 template<typename T>
